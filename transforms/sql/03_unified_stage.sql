da7629a4cf3a1dee667d3cc3de21b8ae7ff82b3c
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-08-09 17:07:18 +0200

    improve unified code

--- transforms/sql/03_unified_stage.sql
+++ transforms/sql/03_unified_stage.sql
@@ -1,4 +1,48 @@
 CREATE OR REPLACE VIEW silver.unified AS
-SELECT * FROM silver.jobspy
+SELECT
+  source,
+  source_id::text                 AS source_id,
+  source_row_url,
+  job_url_direct,
+  title_raw,
+  title_norm,
+  company_raw,
+  company_name,
+  location_raw,
+  city_guess,
+  region_guess,
+  country_guess,
+  date_posted,
+  is_remote,
+  contract_type_raw,
+  salary_min::numeric             AS salary_min,
+  salary_max::numeric             AS salary_max,
+  currency,
+  description_raw,
+  emails_raw
+FROM silver.jobspy
+
 UNION ALL
-SELECT * FROM silver.stepstone;
+
+SELECT
+  source,
+  source_id::text                 AS source_id,
+  source_row_url,
+  job_url_direct,
+  title_raw,
+  title_norm,
+  company_raw,
+  company_name,
+  location_raw,
+  city_guess,
+  region_guess,
+  country_guess,
+  date_posted,
+  is_remote,
+  contract_type_raw,
+  salary_min::numeric             AS salary_min,
+  salary_max::numeric             AS salary_max,
+  currency,
+  description_raw,
+  emails_raw
+FROM silver.stepstone;
