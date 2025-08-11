f6664d3a73538a02160c0bbcdee21bad2b60cb0e
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-08-11 16:18:45 +0200

    updae seed by name

--- transforms/sql/12a_companies_seed_by_name.sql
+++ transforms/sql/12a_companies_seed_by_name.sql
@@ -1,6 +1,6 @@
 INSERT INTO gold.company (name)
 SELECT DISTINCT s.company_name
 FROM silver.unified s
-LEFT JOIN gold.company gc ON gc.name_norm = util.company_name_norm(s.company_name)
+LEFT JOIN gold.company gc ON util.company_name_norm(gc.name) = util.company_name_norm(s.company_name)
 WHERE gc.company_id IS NULL
   AND s.company_name IS NOT NULL AND btrim(s.company_name) <> '';
