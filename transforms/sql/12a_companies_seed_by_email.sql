6003ba8a44f8080246159d614a18df34365c8572
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-08-11 16:58:04 +0200

    u

--- transforms/sql/12a_companies_seed_by_email.sql
+++ transforms/sql/12a_companies_seed_by_email.sql
@@ -4,6 +4,9 @@ WITH cand AS (
     util.company_name_norm(s.company_name) AS name_norm,
     CASE WHEN util.is_generic_email_domain(s.contact_email_root) THEN NULL ELSE s.contact_email_root END AS email_root
   FROM silver.unified s
+  WHERE s.company_name IS NOT NULL
+    AND btrim(s.company_name) <> ''
+    AND util.company_name_norm(s.company_name) IS NOT NULL
 )
 INSERT INTO gold.company (name)
 SELECT cnd.company_name
