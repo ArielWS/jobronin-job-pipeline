6811dd1e138256e3ba972560d63dca2c416c0fa6
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-08-11 16:49:51 +0200

    updte

--- transforms/sql/12a_companies_seed_by_email.sql
+++ transforms/sql/12a_companies_seed_by_email.sql
@@ -4,14 +4,12 @@ WITH cand AS (
     util.company_name_norm(s.company_name) AS name_norm,
     CASE WHEN util.is_generic_email_domain(s.contact_email_root) THEN NULL ELSE s.contact_email_root END AS email_root
   FROM silver.unified s
-  LEFT JOIN gold.company gc
-    ON (s.company_domain IS NOT NULL AND util.same_org_domain(gc.website_domain, s.company_domain))
-    OR (s.company_domain IS NULL AND util.company_name_norm(gc.name) = util.company_name_norm(s.company_name))
-  WHERE gc.company_id IS NULL
-    AND s.company_name IS NOT NULL AND btrim(s.company_name) <> ''
 )
 INSERT INTO gold.company (name)
-SELECT c.company_name
-FROM cand c
-LEFT JOIN gold.company gc ON util.company_name_norm(gc.name) = c.name_norm
-WHERE gc.company_id IS NULL AND c.email_root IS NOT NULL;
+SELECT cnd.company_name
+FROM cand cnd
+WHERE cnd.email_root IS NOT NULL
+  AND NOT EXISTS (
+    SELECT 1 FROM gold.company gc
+    WHERE util.company_name_norm(gc.name) = cnd.name_norm
+  );
