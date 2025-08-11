f62ebb0ea58935533619b902e66443626d6ccc1d
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-08-11 16:58:27 +0200

    u

--- transforms/sql/12a_companies_seed_by_name.sql
+++ transforms/sql/12a_companies_seed_by_name.sql
@@ -1,7 +1,9 @@
 INSERT INTO gold.company (name)
 SELECT DISTINCT s.company_name
 FROM silver.unified s
-WHERE s.company_name IS NOT NULL AND btrim(s.company_name) <> ''
+WHERE s.company_name IS NOT NULL
+  AND btrim(s.company_name) <> ''
+  AND util.company_name_norm(s.company_name) IS NOT NULL
   AND NOT EXISTS (
     SELECT 1 FROM gold.company gc
     WHERE util.company_name_norm(gc.name) = util.company_name_norm(s.company_name)
