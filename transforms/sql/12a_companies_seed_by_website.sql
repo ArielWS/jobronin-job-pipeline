dcefdd12b6c856b61a59fc388546e1650800293a
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-08-11 15:24:09 +0200

    update seed webstie

--- transforms/sql/12a_companies_seed_by_website.sql
+++ transforms/sql/12a_companies_seed_by_website.sql
@@ -2,14 +2,14 @@ WITH cand AS (
   SELECT DISTINCT
     company_name,
     util.company_name_norm(company_name) AS name_norm,
-    NULLIF(company_domain,'') AS website_domain
+    NULLIF(company_domain,'')            AS website_root
   FROM silver.unified
   WHERE company_name IS NOT NULL AND btrim(company_name) <> ''
 )
 INSERT INTO gold.company (name, website_domain)
-SELECT c.company_name, c.website_domain
+SELECT c.company_name, c.website_root
 FROM cand c
 LEFT JOIN gold.company gc
-  ON (c.website_domain IS NOT NULL AND gc.website_domain = c.website_domain)
-  OR (c.website_domain IS NULL AND gc.name_norm = c.name_norm)
+  ON (c.website_root IS NOT NULL AND util.same_org_domain(gc.website_domain, c.website_root))
+  OR (c.website_root IS NULL AND gc.name_norm = c.name_norm)
 WHERE gc.company_id IS NULL;
