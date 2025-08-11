b1de535cc7095199f29cad8005db244ce3a6b775
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-08-11 15:41:55 +0200

    update silver jobspy

--- transforms/sql/01_silver_jobspy.sql
+++ transforms/sql/01_silver_jobspy.sql
@@ -1,4 +1,3 @@
--- transforms/sql/01_silver_jobspy.sql
 CREATE OR REPLACE VIEW silver.jobspy AS
 SELECT
   'jobspy'::text                          AS source,
@@ -27,7 +26,23 @@ SELECT
 
   js.description                          AS description_raw,
   js.emails                               AS emails_raw,
-  util.email_domain(NULLIF(js.emails,''))                   AS contact_email_domain,
-  util.url_host(COALESCE(NULLIF(js.job_url_direct,''), js.job_url)) AS apply_domain
+
+  -- Company website: ignore aggregator hosts; keep full URL + root domain
+  CASE
+    WHEN util.is_aggregator_host(util.url_host(COALESCE(NULLIF(js.company_url_direct,''), NULLIF(js.company_url,''))))
+      THEN NULL
+    ELSE COALESCE(NULLIF(js.company_url_direct,''), NULLIF(js.company_url,''))
+  END AS company_website,
+  CASE
+    WHEN util.is_aggregator_host(util.url_host(COALESCE(NULLIF(js.company_url_direct,''), NULLIF(js.company_url,''))))
+      THEN NULL
+    ELSE util.org_domain(util.url_host(COALESCE(NULLIF(js.company_url_direct,''), NULLIF(js.company_url,''))))
+  END AS company_domain,
+
+  -- Email/apply domains and their org roots
+  util.email_domain(NULLIF(js.emails,''))                                        AS contact_email_domain,
+  util.org_domain(util.email_domain(NULLIF(js.emails,'')))                       AS contact_email_root,
+  util.url_host(COALESCE(NULLIF(js.job_url_direct,''), js.job_url))              AS apply_domain,
+  util.org_domain(util.url_host(COALESCE(NULLIF(js.job_url_direct,''), js.job_url))) AS apply_root
 
 FROM public.jobspy_job_scrape js;
