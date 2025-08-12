d79d1fa996551cf4d86b2a8f1ed89fbee4d74279
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-08-12 06:28:48 +0200

    u

--- transforms/sql/12a_companies_seed_by_website.sql
+++ transforms/sql/12a_companies_seed_by_website.sql
@@ -1,20 +1,23 @@
 WITH cand AS (
   SELECT DISTINCT
-    company_name,
-    util.company_name_norm(company_name) AS name_norm,
-    NULLIF(company_domain,'')            AS website_root
-  FROM silver.unified
-  WHERE company_name IS NOT NULL
-    AND btrim(company_name) <> ''
-    AND util.company_name_norm(company_name) IS NOT NULL
+    s.company_name,
+    util.company_name_norm(s.company_name) AS name_norm,
+    util.org_domain(NULLIF(s.company_domain,'')) AS website_root
+  FROM silver.unified s
+  WHERE s.company_name IS NOT NULL AND btrim(s.company_name) <> ''
+    AND util.company_name_norm(s.company_name) IS NOT NULL
+),
+filtered AS (
+  SELECT * FROM cand
+  WHERE website_root IS NOT NULL
+    AND NOT util.is_aggregator_host(website_root)
+    AND NOT util.is_ats_host(website_root)
 )
 INSERT INTO gold.company (name, website_domain)
-SELECT cnd.company_name, cnd.website_root
-FROM cand cnd
+SELECT f.company_name, f.website_root
+FROM filtered f
 WHERE NOT EXISTS (
-  SELECT 1
-  FROM gold.company gc
-  WHERE
-    (cnd.website_root IS NOT NULL AND util.same_org_domain(gc.website_domain, cnd.website_root))
-    OR (util.company_name_norm(gc.name) = cnd.name_norm)
+  SELECT 1 FROM gold.company gc
+  WHERE gc.name_norm = f.name_norm
+     OR (gc.website_domain IS NOT NULL AND util.same_org_domain(gc.website_domain, f.website_root))
 );
