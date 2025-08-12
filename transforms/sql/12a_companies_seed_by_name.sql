299edefbdc43ae35cb45be547d0a91b860cd9abc
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-08-12 06:56:40 +0200

    u

--- transforms/sql/12a_companies_seed_by_name.sql
+++ transforms/sql/12a_companies_seed_by_name.sql
@@ -1,16 +1,16 @@
 WITH cand AS (
   SELECT DISTINCT
     s.company_name,
-    util.company_name_norm(s.company_name) AS name_norm,
-    util.org_domain(NULLIF(s.company_domain,'')) AS website_root
+    util.company_name_norm(s.company_name)           AS name_norm,
+    util.org_domain(NULLIF(s.company_domain,''))     AS website_root
   FROM silver.unified s
   WHERE s.company_name IS NOT NULL
     AND btrim(s.company_name) <> ''
     AND util.company_name_norm(s.company_name) IS NOT NULL
+    AND NOT util.is_placeholder_company_name(s.company_name)
 ),
 picked AS (
-  SELECT DISTINCT ON (name_norm)
-         company_name, name_norm, website_root
+  SELECT DISTINCT ON (name_norm) company_name, name_norm, website_root
   FROM cand
   ORDER BY name_norm, website_root
 )
@@ -18,10 +18,9 @@ INSERT INTO gold.company (name)
 SELECT company_name
 FROM picked
 WHERE NOT EXISTS (
-  SELECT 1
-  FROM gold.company gc
+  SELECT 1 FROM gold.company gc
   WHERE picked.website_root IS NOT NULL
     AND gc.website_domain IS NOT NULL
     AND util.same_org_domain(gc.website_domain, picked.website_root)
 )
-ON CONFLICT DO NOTHING;
+ON CONFLICT (name_norm) DO NOTHING;
