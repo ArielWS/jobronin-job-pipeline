eb2af75e459593aa2481cdd2db8899ce87f61825
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-08-12 06:29:40 +0200

    u

--- transforms/sql/12a_companies_seed_by_name.sql
+++ transforms/sql/12a_companies_seed_by_name.sql
@@ -1,10 +1,15 @@
 INSERT INTO gold.company (name)
 SELECT DISTINCT s.company_name
 FROM silver.unified s
-WHERE s.company_name IS NOT NULL
-  AND btrim(s.company_name) <> ''
+WHERE s.company_name IS NOT NULL AND btrim(s.company_name) <> ''
   AND util.company_name_norm(s.company_name) IS NOT NULL
   AND NOT EXISTS (
-    SELECT 1 FROM gold.company gc
-    WHERE util.company_name_norm(gc.name) = util.company_name_norm(s.company_name)
+    SELECT 1 FROM gold.company gc WHERE gc.name_norm = util.company_name_norm(s.company_name)
+  )
+  AND NOT EXISTS (
+    SELECT 1
+    FROM gold.company gc
+    JOIN LATERAL util.org_domain(NULLIF(s.company_domain,'')) od(root) ON TRUE
+    WHERE od.root IS NOT NULL AND gc.website_domain IS NOT NULL
+      AND util.same_org_domain(gc.website_domain, od.root)
   );
